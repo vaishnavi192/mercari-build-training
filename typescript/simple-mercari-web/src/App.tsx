4712309a21cc4b0fda42853f53cef603d27440a8
vaishnavi maheshwari <117008958+vaishnavi192@users.noreply.github.com>
2024-03-28 11:03:23 +0530

    Step7

--- typescript/simple-mercari-web/src/App.tsx
+++ typescript/simple-mercari-web/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
     <div>
       <header className='Title'>
         <p>
-          <b>Simple Mercari</b>
+          <b>My First Webpage</b>
         </p>
       </header>
       <div>
